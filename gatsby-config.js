49f76b2b9e0a8bf49edcbb9e0fb8d5af9e8fc015
Mikel-Jon West <dwmyke@gmail.com>
2021-02-06 22:54:08 -0800

    Nothing added to gatsby-config.js. Adjusted for image and manifest.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,22 @@
 module.exports = {
     plugins: [
-
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: `Steven Ray Byrd`,
+          short_name: `steven-ray-byrd`,
+          description: `Steven Ray Byrd - Actor for hire in Albuquerque and Santa Fe, New Mexico.`,
+        lang: `en`,
+        icon: `src/images/icon.png`,
+        theme_color_in_head: false,
+          start_url: `/`,
+          background_color: `#1d1d1d`,
+          theme_color: `#603cba`,
+          display: `standalone`,
+        },
+      },
     ]
 }
